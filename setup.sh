33d624a09b9951ea4c9c7f1f28200cabdd93937f
Jun Pataleta <jun@moodle.com>
2026-07-28 11:02:45 +0800

    Update NVM installer version

--- setup.sh
+++ setup.sh
@@ -245,7 +245,7 @@ source ~/.bashrc
 # php admin/tool/behat/cli/init.php -j=2 -o
 
 # Install nvm.
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.6/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
